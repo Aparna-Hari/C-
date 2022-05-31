e9768cdaae3f626accb952f774c4d8908c817405
Aparna-Hari <101218385+Aparna-Hari@users.noreply.github.com>
2022-05-31 16:56:37 -0700

    celsius degrees to Kelvin and Fahrenheit
    program to convert from celsius degrees to Kelvin and Fahrenheit

---
+++ ConvertTemperature.cs
@@ -0,0 +1,16 @@
+﻿// Write a C# program to convert from celsius degrees to Kelvin and Fahrenheit
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the amount of Celsius :");
+        int celsius = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Kelvin = {0}", celsius + 273);
+        Console.WriteLine("Fahrenheit = {0}", celsius * 18 / 10 + 32);
+    }
+
+}
