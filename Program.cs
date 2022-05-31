c56947adeb6f618c11fce0b5fa9aa30fd18553b3
Aparna-Hari <101218385+Aparna-Hari@users.noreply.github.com>
2022-05-31 16:48:54 -0700

    Sum and Division

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// 2.Write a C# Sharp program to print the sum of two numbers
+
+
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Enter two numbers");
+		 int a = Convert.ToInt32(Console.ReadLine());
+		 int b = Convert.ToInt32(Console.ReadLine());
+		int sum = a + b;
+		int div = a / b;	
+		Console.WriteLine("Sum of two numbers is {0}: ", sum);
+		Console.WriteLine("Division  of two numbers is {0}: ", div);
+	}
+}
+
