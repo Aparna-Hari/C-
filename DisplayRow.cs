6357b3388d88ee8e248d0689f207eb0473fc307c
Aparna-Hari <101218385+Aparna-Hari@users.noreply.github.com>
2022-05-31 16:35:54 -0700

    display four times in a row (separated by blank spaces)

---
+++ DisplayRow.cs
@@ -0,0 +1,15 @@
+﻿//12. Write a C# program to that takes a number as input and display it four times in a row (separated by blank spaces),
+//and then four times in the next row, with no separation. 
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number to display :");
+
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("Output is :\n{0} {0} {0} {0}",number);
+        Console.WriteLine("{0}{0}{0}{0}", number);
+    }
+}
