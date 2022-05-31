a6854040ba41b56c2caf53d0b9168026852ab5ef
Aparna-Hari <101218385+Aparna-Hari@users.noreply.github.com>
2022-05-31 16:27:53 -0700

    Sum of two numbers

---
+++ Sum.cs
@@ -0,0 +1,16 @@
+﻿// 2.Write a C# Sharp program to print the sum of two numbers
+
+
+using System;
+
+ class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Enter two numbers");
+		int a = Console.ReadLine();
+		int b = Console.ReadLine();
+		int sum = a + b;
+		Console.WriteLine("Sum of two numbers is { }: ", sum);
+	}
+}
