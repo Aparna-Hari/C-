489e5e7641b24d59c33bb3ac3f8648f8391ffed0
Aparna-Hari <101218385+Aparna-Hari@users.noreply.github.com>
2022-05-31 16:26:43 -0700

    print Hello and  name in a separate line.

---
+++ Hello.cs
@@ -0,0 +1,13 @@
+﻿//1. Write a C# Sharp program to print Hello and your name in a separate line.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please Enter your name");
+        string UserName = Console.ReadLine();
+        Console.WriteLine("Hello \n{0} " ,UserName);
+    }
+}
