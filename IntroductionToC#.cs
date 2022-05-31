cc181cbcf9b5b9c8693e12005a561ef53c1fabb5
Aparna-Hari <101218385+Aparna-Hari@users.noreply.github.com>
2022-05-31 16:24:50 -0700

    First C# with namespaces , inputs and outputs

---
+++ IntroductionToC#.cs
@@ -0,0 +1,23 @@
+﻿
+using System; 
+//namespace declaration -  namespace is a collection of classes (where rest of the program uses code that is inside the namespacs System)
+
+// Any piece of code we write should be  present inside class
+class Program 
+{
+    //Main method is entry point to your application
+    static void Main() 
+    {
+        //Console class is present inside the namespace System
+        //WriteLine is used to write something to console
+        Console.WriteLine("Please enter your Name");
+
+        //ReadLine is used to read or enter the input
+        string UserName = Console.ReadLine();
+
+        //Console.WriteLine("Hello " + UserName);
+        Console.WriteLine("Hello {0}", UserName);
+    }
+}
+
+// A namespace is used to organize your code and is collection of classes,interfaces,structs,enums and delegates
