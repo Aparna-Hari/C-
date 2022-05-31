d1f7aeb72cfe0afb3af0b13109f710d92926b2f2
Aparna-Hari <101218385+Aparna-Hari@users.noreply.github.com>
2022-05-31 16:31:04 -0700

    Average of 4 numbers

---
+++ Average.cs
@@ -0,0 +1,16 @@
+﻿// 9. Write a C# Sharp program that takes four numbers as input to calculate and print the average
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(" Enter 4 numbers :");
+        double number1 = double.Parse(Console.ReadLine());
+        double number2 = double.Parse(Console.ReadLine());
+        double number3 = double.Parse(Console.ReadLine());
+        double number4 = double.Parse(Console.ReadLine());
+        double avg = (number1 + number2 + number3 + number4) / 4;
+        Console.WriteLine("Average is {0}: ",avg);
+    }
+}
