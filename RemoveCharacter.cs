a8a1e4c60f044280af6c6766ed7678de98196907
Aparna-Hari <101218385+Aparna-Hari@users.noreply.github.com>
2022-06-01 12:42:11 -0700

    Removes a character from string

---
+++ RemoveCharacter.cs
@@ -0,0 +1,16 @@
+﻿//15. Write a C# program remove specified a character from a non-empty string using index of a character.
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(remove_char("W3resource", 1));
+        Console.WriteLine(remove_char("W3resource", 2));
+        Console.WriteLine(remove_char("W3resource", 0));
+    }
+    public static string remove_char(string str, int n)
+    {
+        return str.Remove(n,1);
+    }
+}
