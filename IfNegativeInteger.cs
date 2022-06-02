c528919e88a72706bf462a0046daec859ff37913
Aparna-Hari <101218385+Aparna-Hari@users.noreply.github.com>
2022-06-02 12:58:43 -0700

    check if input is positive or negative

---
+++ IfNegativeInteger.cs
@@ -0,0 +1,24 @@
+﻿// 18. Write a C# program to check two given integers and return true if one is negative and one is positive
+
+using System;
+
+class Program
+{
+    static void  Main()
+    {
+        Console.WriteLine("Enter first integer");
+
+        int firstInteger = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter second integer");
+
+        int secondInteger = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Checking if one is negative and one is positive");
+
+        Console.WriteLine((firstInteger >= 0 && secondInteger <= 0) || (firstInteger <= 0 && secondInteger >= 0));
+            
+            
+
+    }
+}
