c106874d7a1610f7bb746927c076f95262f6c2c4
Aparna-Hari <101218385+Aparna-Hari@users.noreply.github.com>
2022-06-02 12:44:09 -0700

    First character added at the front and back of string

---
+++ AddCharacter.cs
@@ -0,0 +1,17 @@
+﻿// 17. Write a C# program to create a new string from a given string (length 1 or more ) with the first character added at the front and back
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(add_char("The quick brown fox jumps over the lazy dog."));
+    }
+    public static string add_char(string s)
+    {
+        
+        return s.Length > 0
+            ? s.Substring(0,1) + s + s.Substring(0, 1) /* Substring(startIndex, length) */ :s;
+
+    }
+}
