ad04aa0cf31f2fa9ff70e84500e9498e4ff533a7
Aparna-Hari <101218385+Aparna-Hari@users.noreply.github.com>
2022-06-02 13:10:41 -0700

    Triple the sum if inputs are equal
    compute the sum of two given integers, if two values are equal then return the triple of their sum

---
+++ TripleSum.cs
@@ -0,0 +1,23 @@
+﻿// 19. Write a C# program to compute the sum of two given integers, if two values are equal then return the triple of their sum
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter first Integer");
+
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter second Integer");
+
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+        if (firstNumber == secondNumber)
+            Console.WriteLine("Sum is {0}", 3 * firstNumber);
+        else
+            Console.WriteLine("sum is {0}", firstNumber + secondNumber);
+
+    }
+}
