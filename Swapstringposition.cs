099b8b24a90fee78308cd1b98bc8886c401f0c09
Aparna-Hari <101218385+Aparna-Hari@users.noreply.github.com>
2022-06-02 12:21:44 -0700

    Swap position of first and last character in a string

---
+++ Swapstringposition.cs
@@ -0,0 +1,16 @@
+﻿// 16. Write a C# program to create a new string from a given string where the first and last characters will change their positions.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(swap_string("W3resource"));
+    }
+    public static string swap_string(string s)
+    {
+        return s.Length > 1
+            ? s.Substring(s.Length - 1) + s.Substring(1, s.Length - 2) + s.Substring(0,1) : s;
+    }
+}
