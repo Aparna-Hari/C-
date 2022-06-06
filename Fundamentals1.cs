280d540731473e85d5ee90376354a4dce4224175
Aparna-Hari <101218385+Aparna-Hari@users.noreply.github.com>
2022-06-06 11:15:35 -0700

    Update Fundamentals1.cs

--- Fundamentals1.cs
+++ Fundamentals1.cs
@@ -36,24 +36,30 @@ class Program
     //Modify the previous loop to print "Fizz" for multiples of 3, "Buzz" for multiples of 5, and "FizzBuzz" for numbers that are multiples of both 3 and 5
     public static void ForBuzzFizz()
     {
+       
         for (int i = 1; i < 101; i++)
         {
-            if (i % 3 == 0)
+            
+            if (i % 3 == 0 && i% 5 != 0)
             {
                 Console.Write(i);
                 Console.WriteLine(" Fizz");
             }
-            if (i % 5 == 0)
+            else if (i % 5 == 0 && i % 3 != 0)
             {
                 Console.Write(i);
                 Console.WriteLine(" Buzz");
             }
-            if (i % 3 == 0 && i % 5 == 0)
+            else if (i % 3 == 0 && i % 5 == 0)
             {
                 Console.Write(i);
                 Console.WriteLine(" FizzBuzz");
             }
-            
+            else
+            {
+                Console.WriteLine(i);
+            }
+
         }
     }
 
